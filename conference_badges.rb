ea5a537173692174a576e2a0d5a5e5eb6c453802
Bryce Eadie <bryce.eadie@gmail.com>
2018-09-28 05:44:30 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,6 +13,8 @@ end
 def assign_rooms(speakers)
   room_array = []
   speakers.each_with_index{|speaker, index|
-  
+  room_array << "Hello, #{speaker}! You'll be assigned to room #{index + 1}!"
+end
+
   
   
